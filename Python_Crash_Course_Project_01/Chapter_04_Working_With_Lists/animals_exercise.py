ec553e179043e9263b799d72a2507f9b4176a49a
ahamadey27 <hamadey@gmail.com>
2024-01-15 11:34:02 -0500

    small changes

--- Python_Crash_Course_Project_01/Chapter_04_Working_With_Lists/animals_exercise.py
+++ Python_Crash_Course_Project_01/Chapter_04_Working_With_Lists/animals_exercise.py
@@ -1,4 +1,4 @@
 petAnimals = ["dog", "cat", "pig"]
 for animalList in petAnimals:
-    print(f"{animalList} would make a good pet")
-print("These all would make pretty ill pets come to think of it")
+    print(f"A {animalList} would make a good pet.")
+print("These all would make pretty ill pets come to think of it.")
