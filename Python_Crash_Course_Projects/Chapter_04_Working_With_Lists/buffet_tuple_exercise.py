d624bf78c096d0cdbc49b60242a10fe818c56613
ahamadey27 <hamadey@gmail.com>
2024-02-03 12:29:14 -0500

    End of ch4

---
+++ Python_Crash_Course_Projects/Chapter_04_Working_With_Lists/buffet_tuple_exercise.py
@@ -0,0 +1,9 @@
+buffet_selection = ('rice', 'meartballs', 'tuna', 'corn', 'chicken')
+print("Buffet selection: ")
+for modified_buffet in buffet_selection:
+    print(modified_buffet)
+    
+buffet_selection = ('beans', 'peas', 'tuna', 'corn', 'chicken')
+print("\nModified buffet menu: ")
+for modified_buffet in buffet_selection:
+    print(modified_buffet)
