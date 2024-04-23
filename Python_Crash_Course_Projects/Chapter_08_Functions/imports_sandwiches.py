7de33ad0fa9178988bd70a8e86b9ceae9e462455
ahamadey27 <hamadey@gmail.com>
2024-04-23 12:03:43 -0400

    Ch 8 End

---
+++ Python_Crash_Course_Projects/Chapter_08_Functions/imports_sandwiches.py
@@ -0,0 +1,5 @@
+import sandwiches
+from sandwiches import user_toppings
+from sandwiches import user_toppings as ut 
+import sandwiches as sw
+from sandwiches import * 
---
+++ Python_Crash_Course_Projects/Chapter_08_Functions/printing_functions.py
@@ -0,0 +1,7 @@
+import print_script as ps
+
+unprinted_designs = ['TV', 'bad robot', 'big car']
+completed_models = []
+
+ps.print_models(unprinted_designs, completed_models)
+ps.show_completed_models(completed_models)
