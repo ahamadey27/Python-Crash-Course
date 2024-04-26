fcfb1586b1562e6c06cbc30c36dd3dcd79a59be7
ahamadey27 <hamadey@gmail.com>
2024-04-26 11:16:02 -0400

    Ch. 9 Update

--- Python_Crash_Course_Projects/Chapter_09_Classes/login_attempts.py
+++ Python_Crash_Course_Projects/Chapter_09_Classes/login_attempts.py
@@ -7,6 +7,7 @@ class Users:
         self.email = email
         self.zip_code = zip_code
         self.age = age
+        self.login_attempts = 0
     
     def describe_user(self): 
         """Prints summary of user info"""
@@ -17,6 +18,16 @@ class Users:
         """Greets user"""
         msg = f"Hello, {self.first_name} {self.last_name}!"
         print(msg)
+        
+    def increment_login_attempts(self):
+        """Increments ligin attempts by 1"""
+        self.login_attempts += 1
+    
+    def reset_login_attempts(self):
+        """Resets login attempts to zero"""
+        self.login_attempts = 0
+        
+        
     
 user_01 = Users("alex", "hamadey", 38, "hamadey@gmail.com", 12401)
 user_01.describe_user()
@@ -25,3 +36,12 @@ user_01.greet_user()
 user_02 = Users("bill", "samson", 48, "bill.samson@gmail.com", 19801)
 user_02.describe_user()
 user_02.greet_user()
+
+print("\nMaking 3 login attempts")
+user_01.increment_login_attempts()
+user_01.increment_login_attempts()
+user_01.increment_login_attempts()
+print(f"{user_01.first_name} attempted to login {user_01.login_attempts} times")
+
+user_01.reset_login_attempts()
+print(f"\n{user_01.first_name} login attempts have been set back to {user_01.login_attempts}.")
